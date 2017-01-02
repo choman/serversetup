571f786c5a910405a68e67acfb49b14c5f54caf0
Chad <choman@gmail.com>
2017-01-02 17:38:24 -0600

    UPdated to install docker

--- install.sh
+++ install.sh
@@ -37,10 +37,24 @@ printf "\nInstalling base apps\n"
 sudo apt update
 sudo apt install -y apt-fast di axel build-essential
 sudo apt-fast dist-upgrade -y
+sudo apt-fast install -y apt-transport-https ca-certificates
+
+# setup docker
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+
+
 
 printf "\nConfiguring apt-fast\n"
 sudo cp -pv /usr/share/bash-completion/completions/apt-fast /etc/bash_completion.d
 
+# install docker
+sudo apt-fast update && sudo apt-fast install -y docker-engine docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 #
 # determine vbox version
